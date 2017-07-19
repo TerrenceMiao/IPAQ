59d0e9d216e5e6f79dc6fae99a85b43b77f88837
Terrence Miao <TerrenceMiao@users.noreply.github.com>
2017-07-19 18:23:08 +1000

    Integrate controller with service

--- src/main/kotlin/org/paradise/ipaq/controllers/ipaqController.kt
+++ src/main/kotlin/org/paradise/ipaq/controllers/ipaqController.kt
@@ -1,7 +1,9 @@
 package org.paradise.ipaq.controllers
 
 import org.paradise.ipaq.domain.ExperianSearchResult
+import org.paradise.ipaq.services.ExperianService
 import org.springframework.http.MediaType
+import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestMethod
 import org.springframework.web.bind.annotation.RequestParam
@@ -11,14 +13,14 @@ import org.springframework.web.bind.annotation.RestController
  * Created by terrence on 17/7/17.
  */
 @RestController
-class ipaqController {
+class ipaqController(private val experianService: ExperianService) {
 
     @RequestMapping(value = "/Search", method = arrayOf(RequestMethod.GET), produces = arrayOf(MediaType.APPLICATION_JSON_VALUE))
     fun search(@RequestParam(value = "query") query: String? = null,
                @RequestParam(value = "country") country: String? = null,
-               @RequestParam(value = "take", defaultValue = "10") take: Int): ExperianSearchResult {
+               @RequestParam(value = "take", defaultValue = "10") take: Int): ResponseEntity<ExperianSearchResult> {
 
-        return ExperianSearchResult()
+        return experianService.search(query, country, take)
     }
 
 }
--- src/main/kotlin/org/paradise/ipaq/services/ExperianService.kt
+++ src/main/kotlin/org/paradise/ipaq/services/ExperianService.kt
@@ -1,8 +1,47 @@
 package org.paradise.ipaq.services
 
+import org.paradise.ipaq.domain.ExperianSearchResult
+import org.paradise.ipaq.services.rest.RestServiceClient
+import org.slf4j.LoggerFactory
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.http.*
+import org.springframework.stereotype.Service
+
 /**
  * Created by terrence on 17/7/17.
  */
-class ExperianService {
-    
+@Service
+class ExperianService(private val restServiceClient: RestServiceClient,
+                      @param:Value("\${app.experian.api.url}") private val experianApiUrl: String,
+                      @param:Value("\${app.experian.api.token}") private val experianApiToken: String) {
+
+    fun search(query: String?, country: String?, take: Int): ResponseEntity<ExperianSearchResult> {
+
+        LOG.debug("Search address with query [{}], country [{}] and take [{}]", query, country, take)
+
+        val urlStr = String.format(SEARCH_URL_FORMAT, experianApiUrl, query, country, take)
+
+        return restServiceClient.exchange(urlStr, HttpMethod.GET, HttpEntity<Any>(requestHttpHeaders), ExperianSearchResult::class.java)
+    }
+
+    private val requestHttpHeaders: HttpHeaders
+        get() {
+            val requestHeaders = HttpHeaders()
+
+            requestHeaders.accept = listOf<MediaType>(MediaType.APPLICATION_JSON)
+            requestHeaders.set(HTTP_HEADER_AUTH_TOKEN, experianApiToken)
+
+            return requestHeaders
+        }
+
+    companion object {
+
+        private val LOG = LoggerFactory.getLogger(ExperianService::class.java)
+
+        private val HTTP_HEADER_AUTH_TOKEN = "auth-token"
+
+        private val SEARCH_URL_FORMAT = "%s/Search?query=%s&country=%s&take=%s"
+        private val DETAILS_URL_FORMAT = "%s/format?country=%s&id=%s"
+    }
+
 }
