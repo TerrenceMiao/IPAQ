2161480a41fbd4f353fc3a2446b09806d0b1a5bc
Terrence Miao <TerrenceMiao@users.noreply.github.com>
2017-07-22 19:54:17 +1000

    Refactor Redis client more Kotlin like

--- src/main/kotlin/org/paradise/ipaq/services/redis/RedisClient.kt
+++ src/main/kotlin/org/paradise/ipaq/services/redis/RedisClient.kt
@@ -1,7 +1,5 @@
 package org.paradise.ipaq.services.redis
 
-import org.slf4j.LoggerFactory
-import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.data.redis.RedisSystemException
 import org.springframework.data.redis.core.StringRedisTemplate
 import org.springframework.stereotype.Component
@@ -10,49 +8,19 @@ import org.springframework.stereotype.Component
  * Created by terrence on 25/5/17.
  */
 @Component
-class RedisClient {
+class RedisClient(val stringRedisTemplate: StringRedisTemplate) {
 
-    @Autowired
-    private val stringRedisTemplate: StringRedisTemplate? = null
+    val valueOperations = stringRedisTemplate.opsForValue()!!
 
     fun persist(key: String, value: String): String {
 
-        val valueOperations = stringRedisTemplate!!.opsForValue()
-
         valueOperations.set(key, value)
 
-        val persistedValue = valueOperations.get(key)
-
-        if (value != persistedValue) {
-            throw RedisSystemException("Redis Server persistence error", null)
-        }
-
-        LOG.info("Key [{}] and Value [{}] has been successfully persisted into Redis server", key, persistedValue)
-
-        return persistedValue
+        return valueOperations.get(key) ?: throw RedisSystemException("Redis Server persistence error", null)
     }
 
-    fun get(key: String): String {
+    fun get(key: String): String = valueOperations.get(key)
 
-        val valueOperations = stringRedisTemplate!!.opsForValue()
-
-        val persistedValue = valueOperations.get(key)
-
-        LOG.info("Value [{}] has been gotten from Redis server with key [{}]", persistedValue, key)
-
-        return persistedValue
-    }
-
-    fun delete(key: String) {
-
-        stringRedisTemplate!!.delete(key)
-
-        LOG.info("Key [{}] has been deleted from Redis server", key)
-    }
-
-    companion object {
-
-        private val LOG = LoggerFactory.getLogger(RedisClient::class.java)
-    }
+    fun delete(key: String) = stringRedisTemplate.delete(key)
 
 }
