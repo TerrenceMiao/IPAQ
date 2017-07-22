b4d5567b6bbaac5207b525846c6d68b36b871049
Terrence Miao <TerrenceMiao@users.noreply.github.com>
2017-07-22 20:39:54 +1000

    Refactor Redis service more Kotlin like

--- src/main/kotlin/org/paradise/ipaq/services/redis/RedisService.kt
+++ src/main/kotlin/org/paradise/ipaq/services/redis/RedisService.kt
@@ -6,7 +6,6 @@ import com.fasterxml.jackson.databind.JsonMappingException
 import com.fasterxml.jackson.databind.ObjectMapper
 import org.apache.commons.lang3.StringUtils
 import org.slf4j.LoggerFactory
-import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Service
 import java.io.IOException
 
@@ -14,44 +13,43 @@ import java.io.IOException
  * Created by terrence on 30/5/17.
  */
 @Service
-class RedisService {
-
-    @Autowired
-    private val redisClient: RedisClient? = null
-
-    @Autowired
-    private val objectMapper: ObjectMapper? = null
-
+class RedisService(val redisClient: RedisClient, val objectMapper: ObjectMapper) {
 
     fun <T> persist(key: String, json: T): String {
 
-        var jsonAsString: String? = null
-
-        try {
-            jsonAsString = objectMapper!!.writeValueAsString(json)
+        var jsonAsString: String? = try {
+            objectMapper.writeValueAsString(json)
         } catch (e: JsonProcessingException) {
-            LOG.error("Exception thrown while serialising JSON object: " + e.message)
+            LOG.error("Exception thrown while serialising JSON object: [{}]", e.message)
+            null
         }
 
-        return redisClient!!.persist(key, jsonAsString!!)
+        LOG.debug("Persisting key [{}] / value [{}] into Redis server", key, jsonAsString)
+
+        return redisClient.persist(key, jsonAsString!!)
     }
 
-    operator fun <T> get(key: String, type: Class<T>): T? {
+    fun <T> get(key: String, type: Class<T>): T? {
 
-        val persistedValue = redisClient!!.get(key)
+        LOG.debug("Retrieving from Redis server by key [{}]", key)
 
-        if (StringUtils.isNotEmpty(persistedValue)) {
+        val value = redisClient.get(key)
+
+        if (StringUtils.isNotEmpty(value)) {
             try {
-                return objectMapper!!.readValue(persistedValue, type)
-            } catch (e: JsonParseException) {
-                LOG.error("JSON exception thrown while de-serialising object: " + e.message)
-            } catch (e: JsonMappingException) {
-                LOG.error("JSON exception thrown while de-serialising object: " + e.message)
-            } catch (e: IOException) {
-                LOG.error("IO exception thrown while de-serialising object: " + e.message)
+                return objectMapper.readValue(value, type)
+            } catch (ex: Exception) {
+                when (ex) {
+                    is JsonParseException, is JsonMappingException -> {
+                        LOG.error("JSON exception thrown while de-serialising object: [{}]" + ex.message)
+                    }
+                    is IOException -> {
+                        LOG.error("IO exception thrown while de-serialising object: [{}]" + ex.message)
+                    }
+                }
             }
         } else {
-            LOG.error(CANT_FIND_SESSION_IN_CACHE)
+            LOG.error(CANT_FIND_IN_REDIS_SERVER)
         }
 
         return null
@@ -59,12 +57,14 @@ class RedisService {
 
     fun delete(key: String) {
 
-        redisClient!!.delete(key)
+        LOG.debug("Deleting key [{}] from Redis server", key)
+
+        redisClient.delete(key)
     }
 
     companion object {
 
-        val CANT_FIND_SESSION_IN_CACHE = "Can't find session in cache"
+        val CANT_FIND_IN_REDIS_SERVER = "Can't find in Redis server"
 
         private val LOG = LoggerFactory.getLogger(RedisService::class.java)
     }
