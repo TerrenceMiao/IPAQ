ea158e22964371ff8432913f615cb02f8a9b8298
Terrence Miao <TerrenceMiao@users.noreply.github.com>
2017-07-28 10:07:34 +1000

    Add environment dependent variables into EnvironmentLocal object as well as into Sleuth's trace baggage and span

--- src/main/kotlin/org/paradise/ipaq/controllers/ipaqController.kt
+++ src/main/kotlin/org/paradise/ipaq/controllers/ipaqController.kt
@@ -3,29 +3,45 @@ package org.paradise.ipaq.controllers
 import org.paradise.ipaq.domain.ExperianAddress
 import org.paradise.ipaq.domain.ExperianSearchResult
 import org.paradise.ipaq.services.ExperianService
+import org.springframework.cloud.sleuth.Tracer
 import org.springframework.http.MediaType
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestMethod
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
-
 /**
  * Created by terrence on 17/7/17.
  */
 @RestController
-class ipaqController(val experianService: ExperianService) {
+class IpaqController(val experianService: ExperianService, val tracer: Tracer, var environmentLocal: EnvironmentLocal) {
 
     @RequestMapping(value = "/Search", method = arrayOf(RequestMethod.GET), produces = arrayOf(MediaType.APPLICATION_JSON_VALUE))
     fun search(@RequestParam(value = "query") query: String,
                @RequestParam(value = "country") country: String,
-               @RequestParam(value = "take", defaultValue = "10") take: Int): ResponseEntity<ExperianSearchResult>
-            = experianService.search(query, country, take)
+               @RequestParam(value = "take", defaultValue = "10") take: Int): ResponseEntity<ExperianSearchResult> {
 
+        // Save environment dependent variables into request scope bean
+        environmentLocal.query = query
+        environmentLocal.country = country
+
+        tracer.addTag(COUNTRY, country)
+
+        val span = tracer.currentSpan
+        span.setBaggageItem(COUNTRY, country)
+
+        return experianService.search(query, country, take)
+    }
 
     @RequestMapping(value = "/format", method = arrayOf(RequestMethod.GET), produces = arrayOf(MediaType.APPLICATION_JSON_VALUE))
     fun format(@RequestParam(value = "country") country: String,
                @RequestParam(value = "id") id: String): ResponseEntity<ExperianAddress>
             = experianService.format(country, id)
 
+    companion object {
+
+        private val COUNTRY = "Country"
+    }
+
 }
+
