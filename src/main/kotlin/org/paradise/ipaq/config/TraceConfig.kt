736bde3cbae938c250fcebe7a21e2e028912710b
Terrence Miao <TerrenceMiao@users.noreply.github.com>
2017-07-29 23:30:50 +1000

    Add TraceConfig

---
+++ src/main/kotlin/org/paradise/ipaq/config/TraceConfig.kt
@@ -0,0 +1,11 @@
+package org.paradise.ipaq.config
+
+import org.springframework.context.annotation.Configuration
+
+/**
+ * Created by terrence on 29/7/17.
+ */
+@Configuration
+class TraceConfig {
+
+}
