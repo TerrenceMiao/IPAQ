c145eeb47be552b2fcdeaab175693aee4bb4106d
Terrence Miao <TerrenceMiao@users.noreply.github.com>
2017-08-01 09:22:12 +1000

    Make integration test passed through

--- src/test/kotlin/org/paradise/ipaq/integration/IntegrationConfig.kt
+++ src/test/kotlin/org/paradise/ipaq/integration/IntegrationConfig.kt
@@ -1,14 +1,18 @@
 package org.paradise.ipaq.integration
 
+import org.apache.commons.io.IOUtils
+import org.apache.commons.lang3.StringUtils
 import org.mockito.Matchers.any
 import org.mockito.Matchers.anyString
 import org.mockito.Mockito.*
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.core.io.ClassPathResource
 import org.springframework.data.redis.connection.RedisConnection
 import org.springframework.data.redis.connection.RedisConnectionFactory
 import org.springframework.data.redis.core.StringRedisTemplate
 import org.springframework.data.redis.core.ValueOperations
+import java.nio.charset.Charset
 
 /**
  * Created by terrence on 26/5/17.
@@ -35,6 +39,10 @@ class IntegrationConfig {
 
         @Suppress("UNCHECKED_CAST")
         `when`(stringRedisTemplate.opsForValue()).thenReturn(valueOperations as ValueOperations<String, String>?)
+
+        `when`(valueOperations!!.get(eq("1 Infinite Loop, USA")))
+                .thenReturn(StringUtils.EMPTY)
+                .thenReturn(IOUtils.toString(ClassPathResource("search-resp.json").inputStream, Charset.defaultCharset()))
 
         doNothing().`when`<ValueOperations<String, String>>(valueOperations).set(any(String::class.java), any(String::class.java))
 
