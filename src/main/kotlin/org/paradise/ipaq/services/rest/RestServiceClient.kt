cc6da7cb8ddd0ba03f1afa95c57c4ac26400d00a
Terrence Miao <TerrenceMiao@users.noreply.github.com>
2017-08-03 09:12:41 +1000

    Remove ProxyConfig and move beans into RestServiceClient

--- src/main/kotlin/org/paradise/ipaq/services/rest/RestServiceClient.kt
+++ src/main/kotlin/org/paradise/ipaq/services/rest/RestServiceClient.kt
@@ -1,5 +1,12 @@
 package org.paradise.ipaq.services.rest
 
+import org.apache.commons.lang3.math.NumberUtils
+import org.apache.http.HttpHost
+import org.apache.http.auth.AuthScope
+import org.apache.http.auth.UsernamePasswordCredentials
+import org.apache.http.impl.client.BasicCredentialsProvider
+import org.apache.http.impl.client.HttpClientBuilder
+import org.apache.http.impl.client.ProxyAuthenticationStrategy
 import org.springframework.http.HttpEntity
 import org.springframework.http.HttpMethod
 import org.springframework.http.ResponseEntity
@@ -14,17 +21,54 @@ import java.io.IOException
  * RESTful service client, based on Spring RestTemplate and Apache HTTP Client.
  */
 @Component
-class RestServiceClient(httpComponentsClientHttpRequestFactory: HttpComponentsClientHttpRequestFactory) {
+class RestServiceClient {
 
     private val restTemplate: RestTemplate = RestTemplate()
 
     init {
-        restTemplate.requestFactory = httpComponentsClientHttpRequestFactory
+        restTemplate.requestFactory = httpComponentsClientHttpRequestFactory()
         restTemplate.errorHandler = createResponseErrorHandler()
     }
 
     fun <T> exchange(url: String, method: HttpMethod, requestEntity: HttpEntity<*>, responseType: Class<T>, vararg uriVariables: Any): ResponseEntity<T>
             = restTemplate.exchange(url, method, requestEntity, responseType, *uriVariables)
+
+    private fun httpComponentsClientHttpRequestFactory(): HttpComponentsClientHttpRequestFactory {
+
+        /**
+         * Consider using HttpClientBuilder.useSystemProperties() call in Apache HttpClient which use system properties
+         * when creating and configuring default implementations, including http.proxyHost and http.proxyPort.
+
+         * In Apache HTTP Client 5, http.proxyUser and http.proxyPassword also defined in:
+
+         * @see org.apache.hc.client5.http.impl.auth.SystemDefaultCredentialsProvider
+         */
+        val systemProperties = System.getProperties()
+
+        val proxyHost = systemProperties.getProperty(PROXY_HOST_PROPERTY)
+        val proxyPort = NumberUtils.toInt(systemProperties.getProperty(PROXY_PORT_PROPERTY), -1)
+
+        val proxyUser = systemProperties.getProperty(PROXY_USER_PROPERTY)
+        val proxyPassword = systemProperties.getProperty(PROXY_PASSWORD_PROPERTY)
+
+        val httpClientBuilder = HttpClientBuilder.create()
+
+        if (proxyHost != null && proxyPort != -1) {
+            httpClientBuilder.setProxy(HttpHost(proxyHost, proxyPort))
+
+            if (proxyUser != null && proxyPassword != null) {
+                val passwordCredentials = UsernamePasswordCredentials(proxyUser, proxyPassword)
+
+                val credentialsProvider = BasicCredentialsProvider()
+                credentialsProvider.setCredentials(AuthScope(proxyHost, proxyPort), passwordCredentials)
+
+                httpClientBuilder.setDefaultCredentialsProvider(credentialsProvider)
+                httpClientBuilder.setProxyAuthenticationStrategy(ProxyAuthenticationStrategy())
+            }
+        }
+
+        return HttpComponentsClientHttpRequestFactory(httpClientBuilder.build())
+    }
 
     /**
      * By pass the default Exception Handler. When make RESTful call, disable any error handler when use this
@@ -47,6 +91,14 @@ class RestServiceClient(httpComponentsClientHttpRequestFactory: HttpComponentsCl
 
             }
         }
+    }
+
+    companion object {
+
+        private val PROXY_HOST_PROPERTY = "http.proxyHost"
+        private val PROXY_PORT_PROPERTY = "http.proxyPort"
+        private val PROXY_USER_PROPERTY = "http.proxyUser"
+        private val PROXY_PASSWORD_PROPERTY = "http.proxyPassword"
     }
 
 }
